5164dd4d8dd71458bab4e72b314162be44c2df6a
islanddddddd <1207446924@qq.com>
2023-03-03 17:43:51 +0800

    增加了配置选项，增加了自动播报

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ const { Plugin, segment, http } = require("keli");
 const { version } = require("./package.json");
 const plugin = new Plugin("dywoman", version);
 
-const config = {};
+// keyword是关键字，元素类型为字符串
+// timeInterval是自动发送的间隔，单位为小时，默认为6小时发送一次，数组元素为数字
+// enableGroup是启用自动发送的群，只有数组中有的群才会启用自动播报，数组元素为数字
+const config = { keyword: "dywoman", timeInterval: [6], enableGroup: [] };
 
 plugin.onMounted((bot, admins) => {
     plugin.saveConfig(Object.assign(config, plugin.loadConfig()));
@@ -11,17 +14,29 @@ plugin.onMounted((bot, admins) => {
     plugin.onMessage(async (event, bot) => {
         const { raw_message } = event;
 
-        if (raw_message === "dywoman") {
-            const { data } = await http.get(
-                "https://zj.v.api.aa1.cn/api/video_dyv2"
-            );
-            let msgs = [segment.face(66), "world"];
-
-            if (data.msg == "获取成功") {
-                msgs = ["简介：", data.dsc, "/r", data.url];
-            }
-
+        if (raw_message === config.keyword) {
+            const msgs = await getData();
             event.reply(msgs);
+        }
+    });
+
+    async function getData() {
+        const { data } = await http.get(
+            "https://zj.v.api.aa1.cn/api/video_dyv2"
+        );
+        let msgs = [segment.face(66), "获取数据失败"];
+        if (data.msg == "获取成功") {
+            msgs = ["简介：", data.dsc, "/r", data.url];
+        }
+        return msgs;
+    }
+    // 定时播报
+    const task = plugin.cron(`0 */${config.timeInterval} * * *`, async () => {
+        const enableGroup = config.enableGroup;
+        const msgs = await getData();
+        for (let index = 0; index < enableGroup.length; index++) {
+            const element = enableGroup[index];
+            bot.pickGroup(element).sendMsg(msgs);
         }
     });
 });
